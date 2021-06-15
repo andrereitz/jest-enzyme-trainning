3f632562c4839fbb0e743ceb3d057e7430b68584
Andre Reitz <andrereitz@live.com>
2021-06-15 17:59:22 -0300

    19 - implementing enzyme

---
+++ src/App.test.js
@@ -0,0 +1,26 @@
+import App from './App';
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders without error', () => {
+
+});
+
+test('renders button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking on button increments counter display', () => {
+
+});
+
