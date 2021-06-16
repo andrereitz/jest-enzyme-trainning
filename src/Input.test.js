536f778c001a0da54981452dba56bd7a96e13a10
Andre Reitz <andrereitz@live.com>
2021-06-16 16:35:21 -0300

    Adding mock for useState

--- src/Input.test.js
+++ src/Input.test.js
@@ -2,6 +2,14 @@ import { shallow } from 'enzyme';
 
 import { checkProps, findByTestAttr } from './test/testUtils';
 import Input from './Input';
+import React from 'react';
+
+const mockSetCurrentGuess = jest.fn();
+
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useState: (initialState) => [initialState, mockSetCurrentGuess]
+}))
 
 const setup = (secretWord = 'test') => {
     return shallow(<Input secretWord={secretWord} />)
@@ -16,4 +24,16 @@ test('input renders without errors', () => {
 
 test('does not throw warning with expected props', () => {
     checkProps(Input, { secretWord: 'test' });
+})
+
+describe('state controlled input field', () => {
+    test('state updates with value of input box upon change', () => {
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+
+        const mockEvent = { target: { value: 'train' } };
+        inputBox.simulate('change', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    })
 })
