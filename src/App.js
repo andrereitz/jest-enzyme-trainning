d1e0ac1f0e080b376c9e4f20d27df14c57ee9d3a
Andre Reitz <andrereitz@live.com>
2021-06-16 14:12:56 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-  const [error, setError] = useState(null);
-
-  function handleIncrement(){
-    if(error){
-      setError(null);
-    }
-
-    setCount(count + 1);
-  }
-
-  function handleDecrement(){
-    if(count == 0){
-      setError('Error - Can\'t go below 0' );
-    }else{
-      if(error){
-        setError('');
-      }
-
-      setCount(count - 1);
-    }
-  }
-
   return (
-    <div className="App" data-test="component-app">
-      <h1 data-test="counter-display">The counter is currently <span data-test="count">{count}</span></h1>
-      <button onClick={() => handleIncrement()} data-test="increment-button">Increment</button>
-      <button onClick={() => handleDecrement()} data-test="decrement-button">Decrement</button>
-      {error &&
-        <div data-test="error">{error}</div>
-      }
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,70 +1,8 @@
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import Enzyme, { shallow } from 'enzyme'
-import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 
-Enzyme.configure({ adapter: new EnzymeAdapter() })
-
-/**
- * Factory function to create a ShallowEapper - App Component
- * @function setup
- * @returns {ShallowWrapper}
- */
-const setup = () => shallow(<App />)
-
-const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
-
-test('renders without error', () => {
-    const wrapper = setup();
-    const appComponent = findByTestAttr(wrapper, 'component-app')
-
-    expect(appComponent.length).toBe(1)
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
-
-test('renders button', () => {
-    const wrapper = shallow(<App />);
-    const button = findByTestAttr(wrapper, 'increment-button')
-
-    expect(button.length).toBe(1)
-});
-
-test('renders counter display', () => {
-    const wrapper = shallow(<App />);
-    const counterDisplay = findByTestAttr(wrapper, 'counter-display')
-
-    expect(counterDisplay.length).toBe(1)
-});
-
-test('counter starts at 0', () => {
-    const wrapper = setup();
-    const count = findByTestAttr(wrapper, "count").text();
-
-    expect(count).toBe("0")
-
-});
-
-test('clicking on button increments counter display', () => {
-    const wrapper = setup();
-    
-    // Find button
-    const button = findByTestAttr(wrapper, 'increment-button');
-
-    // Click button
-    button.simulate('click');
-
-    // Find display and test if has benn incremented
-    const count = findByTestAttr(wrapper, 'count').text();
-    expect(count).toBe("1")
-});
-
-test('clicking on button decrements counter display', () => {
-    const wrapper = setup();
-
-    const buttonIncrement = findByTestAttr(wrapper, 'increment-button');
-    buttonIncrement.simulate('click');
-
-    const buttonDecrement = findByTestAttr(wrapper, 'decrement-button');
-    buttonDecrement.simulate('click');
-
-    const count = findByTestAttr(wrapper, 'count').text();
-    expect(count).toBe("0")
-})
