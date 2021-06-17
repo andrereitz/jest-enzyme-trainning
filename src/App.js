f5480df6c32411446c74e94422d0c57f48728bf4
Andre Reitz <andrereitz@live.com>
2021-06-17 12:05:12 -0300

    Refactor using reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useReducer } from 'react';
 import './App.css';
 
 import Congrats from './Congrats';
@@ -6,11 +6,29 @@ import GuessedWords from './GuessedWords';
 import Input from './Input';
 import { getSecretWord } from './actions';
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'setSecretWord':
+        return { ...state, secretWord: action.payload }
+    default: 
+      throw new Error(`Inavlid action type: ${action.type}`)
+  }
+}
+
 function App() {
-  const [secretWord, setSecretWord] = useState('');
+  // const [secretWord, setSecretWord] = useState('');
+
+  const [state, dispatch] = useReducer(
+    reducer,
+    { secretWord: '' }
+  )
 
   const success = false;
   const guessedWords = [];
+
+  const setSecretWord = (secretWord) => {
+    dispatch({ type: 'setSecretWord', payload: secretWord });
+  }
 
   useEffect(() => {
     getSecretWord(setSecretWord);
@@ -20,7 +38,7 @@ function App() {
     <div className="container" data-test="component-app">
       <h1>Jotto</h1>
       <Congrats success={success} />
-      <Input success={success} secretWord={secretWord} guessedWords={guessedWords} />
+      <Input success={success} secretWord={state.secretWord} guessedWords={guessedWords} />
       <GuessedWords guessedWords={[ { guessedWord: 'girl', letterMatchCount: 3 } ]} />
     </div>
   );
