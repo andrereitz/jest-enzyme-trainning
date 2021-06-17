49221827a82f9dc69d875c1c4e8e9d71d792d2c4
Andre Reitz <andrereitz@live.com>
2021-06-17 14:30:28 -0300

    Language picker component

---
+++ src/LanguagePicker.js
@@ -0,0 +1,15 @@
+import propTypes from 'prop-types';
+
+function LanguagePicker({ setLanguage }){
+    return(
+        <div data-test="component-language-picker">
+
+        </div>
+    )
+}
+
+LanguagePicker.propTypes = {
+    setLanguage: propTypes.func.isRequired
+}
+
+export default LanguagePicker;
