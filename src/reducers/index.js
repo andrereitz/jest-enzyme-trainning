8b25ec679cfbdc616345a5992333c9dc482b7cfb
Andre Reitz <andrereitz@live.com>
2021-06-18 15:16:53 -0300

    Making tests with redux thunk

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import success from './successReducer';
 import guessedWords from './guessedWord.reducer';
+import secretWord from "./secretWord.reducer";
 
 export default combineReducers({
     success,
-    guessedWords
+    guessedWords,
+    secretWord
 })
