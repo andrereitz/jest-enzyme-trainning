5c7f571f2869c4f0c8f752d60efe9a876ee23f02
Andre Reitz <andrereitz@live.com>
2021-06-18 13:56:47 -0300

    Adding guess word action and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
 export const actionTypes = {
-    CORRECT_GUESS: 'CORRECT_GUESS'
+    CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD'
 }
+
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState) {
+
+    };
+};
 
 export function correctGuess(){
     return { type: actionTypes.CORRECT_GUESS };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import success from './successReducer';
+import guessedWords from './guessedWord.reducer';
 
 export default combineReducers({
     success,
+    guessedWords
 })
